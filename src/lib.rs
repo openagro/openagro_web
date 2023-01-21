d313e753ac1244a2abd910b51e50bc50a35d137e
bluespada <pentingmain@gmail.com>
2023-01-21 08:23:56 +0800

    feat : add web metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use openagro_extension::{ extension::ExtensionMetadata, assets::{ Assets, AssetType } };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn metadata() -> ExtensionMetadata {
+    let mut metadata = ExtensionMetadata {
+        extension_id: "openagro.web.extension".to_string(),
+        name: "Web".to_string(),
+        description: None,
+        category: Some("base".to_string()),
+        author: "OpenAgro Developers".to_string(),
+        version: "1.0.0".to_string(),
+        data: vec![
+            Assets{
+                asset_type: AssetType::Css,
+                assets: include_str!("../assets/css/main.css").to_string(),
+            },
+            Assets{
+                asset_type: AssetType::Javascript,
+                assets: include_str!("../assets/js/main.js").to_string(),
+            },
+            Assets{
+                asset_type: AssetType::Data,
+                assets: include_str!("../data/ir.company.csv").to_string(),
+            }
+        ],
+        depends_on: vec![],
+        application: false,
+    };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    metadata
 }
